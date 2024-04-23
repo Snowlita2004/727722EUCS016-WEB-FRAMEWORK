62009daac2d188fb5cf8acb6cbe7ea5951e89a28
ANGEL SNOWLITA S <124783379+Snowlita2004@users.noreply.github.com>
2024-04-23 09:56:18 +0530

    Add files via upload

---
+++ Day-9/class_exercise/class_exercise_3/springapp/src/main/java/com/example/angel/controller/angelVillageController.java
@@ -0,0 +1,59 @@
+package com.example.springapp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.*;
+import com.example.springapp.model.angelVillage;
+import com.example.springapp.service.angelVillageService;
+
+@RestController
+public class angelVillageController {
+    @Autowired
+    private angelVillageService ser;
+
+    @PostMapping("/api/village")
+    public ResponseEntity<angelVillage> post(@RequestBody angelVillage person) {
+        if (ser.post(person)) {
+            return new ResponseEntity<>(person, HttpStatus.CREATED);
+        } else {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/api/village/byname/{villageName}")
+    public ResponseEntity<angelVillage> getAll(@PathVariable String villageName) {
+        angelVillage li = ser.start(villageName);
+        if (li!=null) {
+            return new ResponseEntity<>(li, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/api/village/bypopulation/{villagePopulation}")
+    public ResponseEntity<List<angelVillage>> getbyAge(@PathVariable int villagePopulation) {
+        List<angelVillage> li = ser.end(villagePopulation);
+        if (li.size() > 0) {
+            return new ResponseEntity<>(li, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/api/village/{villageId}")
+    public ResponseEntity<angelVillage> get3(@PathVariable int villageId) {
+        angelVillage li = ser.get3(villageId);
+        if (li!=null) {
+            return new ResponseEntity<>(li, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+}
